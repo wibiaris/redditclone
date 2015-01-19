141da4c3997e86cc4df520bf9df49b0c87bc87ab
wibiaristama <wibi.aristama@gmail.com>
2015-01-19 23:21:19 +0700

    Add devise and create User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :links
+  resources :comments
+
+  devise_for :users
+  resources :links do
+    member do
+      put "like", to:    "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+    resources :comments
+  end
+  root "links#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
